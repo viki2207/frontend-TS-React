2410be27d3d27c1101a7890227f2866925bf2bf0
Vidhi Shah <shahvidhi1995@gmail.com>
2024-02-28 18:48:29 +1100

    front-end using ts and react

---
+++ src/context/theme.context.tsx
@@ -0,0 +1,16 @@
+import{createContext,useState} from "react";
+interface IThemeContextInterface{
+    darkMode:boolean;
+    toggleDarkMode:() =>void;
+}
+export const ThemeContext = createContext<IThemeContextInterface>({
+    darkMode:false,
+    toggleDarkMode:()=>{},
+});
+// const ThemeContextProvider =({children}) =>{
+//     const[darkMode,setDarkMode] = useState<boolean>(false);
+//     const toggleDarkMode:()=>void = ()=>{
+//         setDarkMode((prevState)=> !prevState)
+//     }
+
+}
